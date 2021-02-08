1e6d3a16d1ae301e623a9b978cfdd09e51380525
Emily Wuenstel <emily.wuenstel@gmail.com>
2021-02-07 21:30:43 -0600

    halfway done with weather

---
+++ vacationpy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "00e181b4e35294370a45258f71514f3c"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
--- weatherpy/api_keys.py
+++ weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bde191f3a1ce139abd3a45a3394aec28"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
